f0f3078141da31dd2f7a86c1bea85fc8492cca87
lvillavicencio6 <lvillavicencio6@gmail.com>
2024-11-21 11:01:43 -0800

    updated the party package products

--- party-packages-products.js
+++ party-packages-products.js
@@ -2,31 +2,31 @@ const products = [
     {
         name: "Red Green & Yellow Package",
         price: "$150",
-        image: "Product images/minijumper.png",
+        image: "content images/red-green-yellow-package.png",
         link: "red-green-yellow-package-product.html"
     },
     {
         name: "Blue & Pink Package",
         price: "$150",
-        image: "Product images/minijumperpink.png",
+        image: "content images/blue-pink-package.png",
         link: "blue-pink-package-product.html"
     },
     {
         name: "Classic Combo Slide Package",
         price: "$260",
-        image: "Product images/minijumperpink.png",
+        image: "content images/classic-combo-slide-package.png",
         link: "classic-combo-slide-package-product.html"
     },
     {
         name: "Pink Castle Combo Slide Package",
         price: "$260",
-        image: "Product images/minijumper.png",
+        image: "content images/pink-castle-combo-slide-package.png",
         link: "pink-castle-combo-slide-package-product.html"
     },
     {
         name: "Tropical Sun Combo Slide Package",
         price: "$260",
-        image: "Product images/minijumperpink.png",
+        image: "content images/tropical-sun-combo-slide-package.png",
         link: "tropical-sun-combo-slide-package-product.html"
     },
     
@@ -34,25 +34,25 @@ const products = [
     {
         name: "Blue Green & Yellow Package",
         price: "$180",
-        image: "Product images/minijumper.png",
+        image: "content images/blue-green-yellow-package.png",
         link: "blue-green-yellow-package-product.html"
     },
     {
         name: "Balloons Package",
         price: "$180",
-        image: "Product images/minijumperpink.png",
+        image: "content images/balloons-package.png",
         link: "balloons-package-product.html"
     },
     {
         name: "Pink Package",
         price: "$180",
-        image: "Product images/minijumper.png",
+        image: "content images/pink-package.png",
         link: "pink-package-product.html"
     },
     {
         name: "Tropical Sun Package",
         price: "$180",
-        image: "Product images/minijumper.png",
+        image: "content images/tropical-sun-package.png",
         link: "tropical-sun-package-product.html"
     },
 ];
