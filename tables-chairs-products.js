aed4e45894bb57aa138c8ebd338ba94abf0ddab8
lvillavicencio6 <lvillavicencio6@gmail.com>
2024-11-21 13:28:37 -0800

    Updated the table and chairs category with the new images

--- tables-chairs-products.js
+++ tables-chairs-products.js
@@ -3,31 +3,31 @@ const products = [
         name: "White Folding Chair",
         price: "$1.50",
         image: "content images/white-folding-chair.png",
-        link: "red-green-yellow-package-product.html"
+        link: "white-folding-chair-product.html"
     },
     {
         name: "6ft Rectangular Table",
         price: "$8",
         image: "content images/6ft-rectangular-table.png",
-        link: "blue-pink-package-product.html"
+        link: "6ft-rectangular-table-product.html"
     },
     {
         name: "Kids Folding Chair",
         price: "$1.50",
         image: "content images/kids-folding-chair.png",
-        link: "classic-combo-slide-package-product.html"
+        link: "kids-folding-chair.html"
     },
     {
         name: "Kids Rectangular Table",
         price: "$8",
         image: "content images/kids-rectangular-table.png",
-        link: "pink-castle-combo-slide-package-product.html"
+        link: "kids-rectangular-table.html"
     },
     {
         name: "60 Inch Round Table",
         price: "$12",
         image: "content images/60-inch-round-table.png",
-        link: "tropical-sun-combo-slide-package-product.html"
+        link: "60-inch-round-table.html"
     },
     
     // ... add more products as needed
